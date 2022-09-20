122576e004306dacca580c87f8b1e9dec734fe66
Daniel Olano <me@olanod.com>
2022-09-20 23:53:22 +0200

    Make pixelated box a custom element

--- shell/public/web_components/prompt.js
+++ shell/public/web_components/prompt.js
@@ -3,37 +3,15 @@ const html = (ss, ...parts) => p.parseFromString('<template>' + parts
 	.reduce((t, val, i) => `${t}${strings[i]}${val}`, '')
 	.concat(ss[parts.length]) + '</template>', 'text/html').querySelector('template');
 
-const template = html`
+const promptTpl = html`
 <style>
 :host {
   --bg: var(--surface-2);
-  --border: var(--brand);
-  --border-focus: var(--violet-3);
-  --clip: var(--pixel-corners, none);
 }
-
-#wrap {
-  padding: var(--font-size-fluid-0);
-}
-#border {
-  clip-path: var(--clip);
-  background: linear-gradient(to right, var(--border) 40%, var(--border-focus) 60%);
-  background-size: 240% 100%;
-  background-position: 0%;
-  transition: background-position 500ms;
-  padding: 2px;
-}
-:host(:focus) #border { background-position: 100%; }
-#prompt {
-  display: flex;
-  background: var(--bg);
-  box-sizing: border-box;
-  padding: var(--font-size-fluid-0) var(--font-size-fluid-1);
-  clip-path: var(--clip);
-  filter: drop-shadow(0 0 1px var(--brand));
-  color: white;
-}
+:host(:focus) fido-box { --box-active: 100%; }
+#wrap { padding: 0.5em var(--font-size-fluid-0); }
 #text-entry {
+  color: white;
   flex: 10;
   min-height: var(--font-size-fluid-1);
   line-height: var(--font-size-fluid-1);
@@ -42,15 +20,14 @@ const template = html`
   text-align: start;
   outline: none;
   margin: 0;
+  padding: 0.5em var(--font-size-fluid-0);
 }
 </style>
 <div id="wrap">
-  <div id="border">
-    <div id="prompt">
-      <slot></slot>
-      <pre id="text-entry" contenteditable><br></pre>
-    </div>
-  </div>
+  <fido-box>
+    <slot></slot>
+    <pre id="text-entry" contenteditable><br></pre>
+  </fido-box>
 </div>
 `;
 
@@ -70,7 +47,7 @@ export class Prompt extends HTMLElement {
     super();
     
 		this.#$root = this.attachShadow({ mode: 'closed', delagatesFocus: true});
-		this.#$root.append(template.content.cloneNode(true))
+		this.#$root.append(promptTpl.content.cloneNode(true));
     this.#$entry = this.#$root.getElementById('text-entry');
 
 		if ('ElementInternals' in window && 
@@ -81,7 +58,7 @@ export class Prompt extends HTMLElement {
   }
   
   connectedCallback() {
-    this.#$root.addEventListener('keypress', this.#enterSubmitOrNewLine)
+    this.#$root.addEventListener('keypress', this.#enterSubmitOrNewLine);
   }
   
   #enterSubmitOrNewLine = e => {
@@ -102,7 +79,7 @@ export class Prompt extends HTMLElement {
   #addNewLine = () => this.#$entry.append(document.createTextNode('\n'));
 
   formResetCallback() {
-    this.#$entry.innerHTML = '<br>';
+    this.#$entry.innerHTML = '\n';
   }
 
 	// form associated element
@@ -117,3 +94,49 @@ export class Prompt extends HTMLElement {
 	reportValidity() { return this.#internals?.reportValidity(); }
 }
 customElements.define(Prompt.TAG, Prompt);
+
+const boxTpl = html`
+<style>
+:host {
+  --box-border: var(--brand);
+  --box-border-active: var(--violet-3);
+  --box-active: 0%;
+  --clip: var(--pixel-corners, none);
+}
+#border {
+  clip-path: var(--clip);
+  background: linear-gradient(to right, var(--box-border) 40%, var(--box-border-active) 60%);
+  background-size: 240% 100%;
+  background-position: var(--box-active);
+  transition: background-position 500ms;
+  padding: 2px;
+}
+#content {
+  background: var(--bg);
+  clip-path: var(--clip);
+  display: flex;
+}
+</style>
+<div id="border">
+  <div id="content">
+    <slot></slot>
+  </div>
+</div>
+`
+
+/**
+ * A box with a pixelated border
+ */
+export class Box extends HTMLElement {
+  static TAG = 'fido-box';
+	static observedAttributes = [];
+
+  #$root;
+
+  constructor() {
+    super();
+		this.#$root = this.attachShadow({ mode: 'closed'});
+		this.#$root.append(boxTpl.content.cloneNode(true))
+  }
+}
+customElements.define(Box.TAG, Box);
