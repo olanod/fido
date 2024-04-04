71c6b949f32670bdd774621eadc9899c40eab03e
b-avb <brayan.vargas.dev@gmail.com>
2024-04-04 13:35:53 -0500

    return an Element instead LazyNodes

--- src/components/atoms/icons/warning.rs
+++ src/components/atoms/icons/warning.rs
@@ -2,14 +2,15 @@ use dioxus::prelude::*;
 
 use super::icon::IconShape;
 
+#[derive(PartialEq, Clone)]
 pub struct Warning;
 impl IconShape for Warning {
     fn view_box(&self) -> String {
         String::from("0 0 24 24")
     }
-    fn child_elements(&self) -> LazyNodes {
-        rsx!(path {
-            d: "M12 8.45v4M12 21a9 9 0 1 1 0-18 9 9 0 0 1 0 18Zm.05-5.55v.1h-.1v-.1h.1Z"
-        })
+    fn child_elements(&self) -> Element {
+        rsx!(
+            path { d: "M12 8.45v4M12 21a9 9 0 1 1 0-18 9 9 0 0 1 0 18Zm.05-5.55v.1h-.1v-.1h.1Z" }
+        )
     }
 }
