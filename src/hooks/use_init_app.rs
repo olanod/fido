156092d7f4c0add60dc7d6da71513f56b2428a73
Brayan Vargas <86427419+b-avb@users.noreply.github.com>
2024-04-09 14:25:21 -0500

    Room interaction (#66)
    * feat: room interaction
    
    * resolve conflict

--- src/hooks/use_init_app.rs
+++ src/hooks/use_init_app.rs
@@ -13,6 +13,8 @@ use ruma::api::client::uiaa::AuthType;
 
 use super::use_auth::CacheLogin;
 use super::use_notification::NotificationItem;
+use super::use_public::PublicState;
+use super::use_room_preview::PreviewRoom;
 use super::use_rooms::RoomsList;
 use super::use_send_attach::SendAttachStatus;
 use super::use_session::UserSession;
@@ -41,6 +43,7 @@ pub fn use_init_app(cx: &ScopeState) {
     // change when we push a ChatRoom from a different nest route
 
     use_shared_state_provider::<CurrentRoom>(cx, || CurrentRoom::default());
+    use_shared_state_provider::<PreviewRoom>(cx, || PreviewRoom::default());
     use_shared_state_provider::<RoomsList>(cx, || RoomsList::default());
     use_shared_state_provider::<Messages>(cx, || Vec::new());
     use_shared_state_provider::<Option<AttachFile>>(cx, || None);
@@ -60,4 +63,5 @@ pub fn use_init_app(cx: &ScopeState) {
         value: HashMap::new(),
     });
     use_shared_state_provider(cx, || SendAttachStatus::Loading(0));
+    use_shared_state_provider::<PublicState>(cx, || PublicState::default());
 }
